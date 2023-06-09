9136ac3ce345e249dd15170ac88599ed8e965ae4
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-06-09 12:01:30 +0100

    .

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
+names = [i for i in dir(hidden_4) if i[:2] != "__"]
+msg = "\n".join(names) if names else ""
 if __name__ == "__main__":
-    print("\n".join([i for i in dir(hidden_4) if i[:2] != "__"]))
+    print("\n".join(names) if names else ""
