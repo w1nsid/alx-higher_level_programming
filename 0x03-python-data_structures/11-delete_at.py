b8ae647fd12a446633554d54abd8576c30ef9bdf
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-06-11 23:09:32 +0100

    .

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    c = (0 <= idx < len(my_list))
-    my_list[idx:idx+1] = [] if c else my_list
+    if (0 <= idx < len(my_list)) and (my_list is not None):
+        del my_list[idx]
     return my_list
