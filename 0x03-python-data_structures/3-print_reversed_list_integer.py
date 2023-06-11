e7c39bad58c71bbea775efa77e032c1f7dafceff
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-06-11 22:28:31 +0100

    .

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    [print("{:d}".format(i)) for i in reversed(my_list)]
+    [print("{:d}".format(i)) for i in reversed(my_list)] if my_list else None
