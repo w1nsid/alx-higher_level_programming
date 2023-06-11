b1e60ecfd0c5917dcd2a7eed331a6996923c9d81
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-06-11 22:14:13 +0100

    .

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    f = (0 <= idx < len(my_list))
-    return my_list[:idx] + [element] + my_list[idx + 1:] if f else my_list
+    if (0 <= idx < len(my_list)):
+        my_list[idx] = element
+    return my_list
