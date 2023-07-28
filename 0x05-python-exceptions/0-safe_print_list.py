9d52a78251920747faceab184651564e30c41880
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-07-28 13:43:54 +0100

    .

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i, item in enumerate(my_list):
         try:
             if i < x:
-                print(item, end=" ")
+                print(item)
                 count += 1
             else:
                 break
