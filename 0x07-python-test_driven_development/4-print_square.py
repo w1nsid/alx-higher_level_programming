8ee0829c821d48e02d5bf58a5af417f9236034bd
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-07-28 21:41:48 +0100

    .

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,20 +2,20 @@
 """This module contains a function to render a square using the # symbol."""
 
 
-def print_square(dimension):
+def print_square(size):
     """Display a square shape using the # character.
 
     Args:
-        dimension (int): The height/width of the square to be printed.
+        size (int): The height/width of the square to be printed.
     Raises:
-        TypeError: If dimension is not an integer.
-        ValueError: If dimension is < 0
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
-    if not isinstance(dimension, int):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    if dimension < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
 
-    for idx in range(dimension):
-        [print("#", end="") for jdx in range(dimension)]
+    for idx in range(size):
+        [print("#", end="") for jdx in range(size)]
         print("")
