92147ba8e545edd382e15938fd8725621a539f68
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-07-28 13:48:40 +0100

    .

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num_printed += 1
+        except (IndexError, ValueError, TypeError):
+            continue
+    print()
+    return num_printed
