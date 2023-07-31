794f1ea9dff0f7ea44707f919e747d173a0f7199
w1nsid <61118207+w1nsid@users.noreply.github.com>
2023-07-31 17:12:03 +0100

    .

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -9,7 +9,8 @@ def is_kind_of_class(obj, a_class):
         obj (any): The object to examine.
         a_class (type): The class to compare with the type of obj.
     Returns:
-        True if obj is an instance or derived instance of a_class, otherwise False.
+        True if obj is an instance or derived instance of a_class, otherwise 
+        False.
     """
     if isinstance(obj, a_class):
         return True
